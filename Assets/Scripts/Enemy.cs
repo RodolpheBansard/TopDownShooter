07f6c86d7f2c1ce469d24964511f3b4898a43b0a
Rodolphe Bansard <rodolphebansard@yahoo.com>
2020-02-07 13:38:39 +0100

    colors

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,15 +6,22 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] int moveSpeed = 3;
     [SerializeField] int health = 3;
+    [SerializeField] Color[] colors;
 
     private Player player;
     private GameSession gameSession;
+    private SpriteRenderer sprite;
 
     
     void Start()
     {
         player = FindObjectOfType<Player>();
         gameSession = FindObjectOfType<GameSession>();
+
+        int indexColor = Random.Range(0, colors.Length);
+        GetComponent<SpriteRenderer>().color = colors[indexColor];
+
+        
     }
 
     
